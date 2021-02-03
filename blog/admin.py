2edb5f0ede552ee8286996db84dc94572331c216
Nataliesmyth <nataliesmyth11@gmail.com>
2021-02-02 23:05:28 -0500

    database models complete

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,13 @@
-from django.contrib import admin
-from .models import Post 
-
 # Register your models here.
 
-admin.site.register(Post)
+from django.contrib import admin
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status','created_on')
+    # list_filter method allows us to filter posts depending on their status
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+  
+admin.site.register(Post, PostAdmin)
